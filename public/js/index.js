7c8a52533fdc38fc99f753736b59ef368cd3ea8d
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-01-26 15:40:18 -0600

    makes a square

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+const topbar = document.getElementById("top-bar")
+
+const canvas = document.getElementById("line-canvas")
+
+const ctx = canvas.getContext("2d")
+
+ctx.fillStyle = "#f00"
+
+ctx.fillRect(0, 0, 100, 100)
