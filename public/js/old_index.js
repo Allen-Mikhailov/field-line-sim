dc1f4cbe7a919fac0614f3e279dcb249e71024ff
SlinkyShelf <77686856+SlinkyShelf@users.noreply.github.com>
2024-01-31 23:50:19 -0600

    Starting to work on the javascript side again. Testing transfering the charge data

---
+++ public/js/old_index.js
@@ -0,0 +1,346 @@
+const topbar = document.getElementById("top-bar")
+
+const canvas = document.getElementById("line-canvas")
+const decor_canvas = document.getElementById("decor-canvas")
+
+import init, { greet } from "/pkg/field_line_sim.js";
+init().then(() => {
+    console.log(greet("WebAssembly"));
+});
+
+// Based on X
+let view_size = 50
+let view_radius = view_size/2
+
+let width, height
+
+let unitPerPixel
+let hWidth, hHeight
+let canvas_bounds
+
+let step_distance = .005
+let record_steps = 60
+let steps_per_record = 200
+
+let neg_charges = []
+
+console.log("Updatinggggg")
+
+
+function update_screen_size()
+{
+    width = canvas.offsetWidth
+    height = canvas.offsetHeight
+
+    canvas_bounds = canvas.getBoundingClientRect()
+
+    console.log(width, height)
+
+    canvas.setAttribute("width", width)
+    canvas.setAttribute("height", height)
+
+    decor_canvas.setAttribute("width", width)
+    decor_canvas.setAttribute("height", height)
+
+    hWidth = width/2 
+    hHeight = height/2 
+
+    unitPerPixel = (width/2)/view_size
+}
+
+update_screen_size()
+
+const ctx = canvas.getContext("2d")
+const decor_ctx = decor_canvas.getContext("2d")
+
+const k = 1 //8.99 * Math.pow(10, 9)
+const nano = Math.pow(10, -9)
+
+const point_charge_size = 30
+const point_charge_start = point_charge_size/unitPerPixel
+const charge_density = 10
+const close_spawn_distance = .01
+
+const arrow_size = 10
+const arrow_angle = Math.PI/4
+
+const charges = {
+    "test-charge": {
+        x: 0,
+        y: 0,
+        q: -1,
+        type: 0,
+    },
+}
+
+for (let i = 0; i < 3; i++)
+{
+    const newCharge = {
+        x: (Math.random()-.5)*2 * 35,
+        y: (Math.random()-.5)*2 * 20,
+        q: 1,
+        type: 0
+    }
+    charges[""+i] = newCharge
+}
+
+console.log(charges)
+
+function getScreenPos(x, y)
+{
+    return [
+        x*unitPerPixel+hWidth, 
+        hHeight-y*unitPerPixel
+    ]
+}
+
+function getScreenPosA([x, y])
+{
+    return getScreenPos(x, y)
+}
+
+function getSpacePos(x, y)
+{
+    return [
+        (x-hWidth)/unitPerPixel,
+        -(y-hHeight)/unitPerPixel
+    ]
+}
+
+function getChargeColor(q)
+{
+    return q > 0?"#00f":"#f00"
+}
+
+function drawArrow(context, sx, sy, angle, style)
+{
+    const [x, y] = getScreenPos(sx, sy)
+
+    context.fillStyle = style
+    context.beginPath();
+    context.moveTo(x, y);
+
+    context.lineTo(
+        x+Math.cos(angle + Math.PI-arrow_angle)*arrow_size,
+        y-Math.sin(angle + Math.PI-arrow_angle)*arrow_size
+        )
+
+    context.lineTo(
+        x+Math.cos(angle - Math.PI+arrow_angle)*arrow_size,
+        y-Math.sin(angle - Math.PI+arrow_angle)*arrow_size
+        )
+
+    context.fill(); // Render the path
+
+}
+
+function drawCharges()
+{
+    Object.keys(charges).map((key) => {
+        const obj = charges[key]
+
+        const screen_pos = getScreenPos(obj.x, obj.y)
+
+        switch (obj.type)
+        {
+            // Point Charge
+            case 0:
+                ctx.strokeStyle = getChargeColor(obj.q)
+                ctx.lineWidth = 2
+                ctx.setLineDash([5, 5]);
+                ctx.beginPath();
+                ctx.arc(Math.floor(screen_pos[0]), Math.floor(screen_pos[1]), point_charge_size, 0, Math.PI*2)
+                ctx.closePath()
+                ctx.stroke();
+                // ctx.fill();
+                break
+                
+        }
+    })
+}
+
+function getPointChargeFieldLinePoint(charge, alpha)
+{
+    return [
+        Math.cos(2*Math.PI*alpha)*close_spawn_distance + charge.x,
+        Math.sin(2*Math.PI*alpha)*close_spawn_distance + charge.y,
+    ]
+}
+
+function getFieldLinePoints(charge)
+{
+    if (charge.q < 0)
+        return []
+
+    let funct;
+    switch (charge.type)
+    {
+        // Point Charge
+        case 0:
+            funct = getPointChargeFieldLinePoint
+            break;
+            
+    }
+
+    const points = []
+    const lineCount = charge.q * charge_density;
+    for (let i = 0; i < lineCount; i++)
+    {
+        const alpha = i/lineCount;
+        points.push(funct(charge, alpha))
+    }
+
+    return points
+}
+
+function getField(x, y)
+{
+    let fx = 0
+    let fy = 0
+    Object.keys(charges).map((key) => {
+        const charge = charges[key]
+        switch (charge.type) 
+        {
+            case 0:
+                const angle = Math.atan2(y-charge.y, x-charge.x)
+                const mag = k*charge.q/Math.hypot(y-charge.y, x-charge.x)
+                fx += mag * Math.cos(angle)
+                fy += mag * Math.sin(angle)
+                break;
+        }
+    })
+
+    return [fx, fy]
+}
+
+const point_charge_distance = point_charge_size/unitPerPixel
+console.log(point_charge_distance)
+function inNegative(x, y)
+{
+    let in_neg = false
+    neg_charges.map((charge) => {
+        if (Math.hypot(x-charge.x, y-charge.y) <= point_charge_distance)
+            in_neg = true
+    })
+    return in_neg
+}
+
+function getFieldLine([sx, sy])
+{
+    const new_line = [[sx, sy]]
+    let x = sx
+    let y = sy
+
+    for (let i = 0; i < record_steps; i++)
+    {
+        const past_steps = []
+        for (let j = 0; j < steps_per_record; j++)
+        {
+            const [fx, fy] = getField(x, y)
+            const angle = Math.atan2(fy, fx)
+            x += Math.cos(angle)*step_distance
+            y += Math.sin(angle)*step_distance
+
+            past_steps.push([x, y])
+        }
+
+        if (inNegative(x, y))
+        {
+            let lastStep = past_steps.length-1
+            
+            while (lastStep > 0 && inNegative(past_steps[lastStep][0], past_steps[lastStep][1]))
+            {
+                lastStep--;
+            }
+            x = past_steps[lastStep][0]
+            y = past_steps[lastStep][1]
+        }
+
+
+        new_line.push([x, y])
+    }
+
+    return new_line
+}
+
+function drawFieldLines() 
+{
+    Object.keys(charges).map((key) => {
+        const charge = charges[key]
+        const start_points = getFieldLinePoints(charge)
+        const space_lines = start_points.map(getFieldLine)
+
+        space_lines.map(line => {
+            const screen_line = line.map(getScreenPosA)    
+
+            ctx.strokeStyle = "white"
+            ctx.lineWidth = .5
+            ctx.beginPath()
+            ctx.moveTo(screen_line[0][0], screen_line[0][1])
+            for (let i = 1; i < screen_line.length; i++)
+            {
+                ctx.lineTo(screen_line[i][0], screen_line[i][1])
+            }
+            // ctx.closePath()
+            ctx.stroke()
+
+            const lastPoint = line[line.length-1]
+            const field = getField(lastPoint[0], lastPoint[1])
+
+            drawArrow(ctx, lastPoint[0], lastPoint[1], Math.atan2(field[1], field[0]), "white")
+        })
+        
+    })
+}
+
+function clearContext(context)
+{
+    context.clearRect(0, 0, width, height);
+}
+
+function update()
+{
+    neg_charges = []
+    Object.keys(charges).map((key) => {
+        const charge = charges[key]
+        if (charge.q < 0)
+            neg_charges.push(charge)
+    })
+
+    clearContext(ctx)
+    drawFieldLines()
+    drawCharges()
+}
+
+update()
+
+const mouse_field_distance = 40
+document.body.onmousemove = (e) => {
+    const x = e.clientX-canvas_bounds.x
+    const y = e.clientY-canvas_bounds.y
+
+    const space_pos = getSpacePos(x, y)
+
+    const field = getField(space_pos[0], space_pos[1])
+    const c = Math.hypot(field[0], field[1])
+    const normal_field = [field[0]/c, field[1]/c]
+
+    clearContext(decor_ctx)
+    decor_ctx.strokeStyle = "red"
+    decor_ctx.beginPath(); // Start a new path
+    decor_ctx.moveTo(x, y); // Move the pen to (30, 50)
+
+    const nx = x+normal_field[0]*mouse_field_distance
+    const ny = y-normal_field[1]*mouse_field_distance
+
+    decor_ctx.lineTo(nx, ny); 
+    decor_ctx.stroke(); // Render the path
+
+    const end_space_pos = getSpacePos(nx, ny)
+
+    // drawArrow(decor_ctx, 0, 0, Math.PI/2, "red")
+    drawArrow(decor_ctx, 
+        end_space_pos[0], 
+        end_space_pos[1], 
+        Math.atan2(normal_field[1], normal_field[0]), "red")
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 // wasm-pack build --target web
 use std::vec;
-use std::f32::consts;
+use std::f32::consts::PI;
+use js_sys::{Array, Object};
+use wasm_bindgen::JsValue;
+use wasm_bindgen::JsCast;
+use js_sys::Reflect;
 
 use wasm_bindgen::prelude::*;
 
@@ -27,15 +31,6 @@ pub struct Vector2
 }
 
 #[wasm_bindgen]
-#[derive(Clone, Copy)]
-pub struct SimData
-{
-    record_frames: u32,
-    step_distance: f32,
-    steps_per_record: u32,
-}
-
-#[wasm_bindgen]
 impl Vector2 
 {
     pub fn new(x: f32, y: f32) -> Vector2 {
@@ -45,17 +40,32 @@ impl Vector2
     pub fn from_angle(a: f32, mag: f32) -> Vector2
     { return Vector2 { x: (a.cos()*mag), y: (a.sin()*mag) } }
 
-    pub fn add(&mut self, v2: &Vector2)
+    pub fn mag(&self) -> f32
+    { return f32::hypot(self.x, self.y); }
+
+    pub fn add_self(&mut self, v2: &Vector2)
     { self.x += v2.x; self.y += v2.y; }
 
-    pub fn sub(&mut self, v2: &Vector2)
+    pub fn add(&self, v2: &Vector2) -> Vector2
+    { return Vector2 {x: self.x + v2.x, y: self.y + v2.y}; }
+
+    pub fn sub_self(&mut self, v2: &Vector2)
     { self.x -= v2.x; self.y -= v2.y; }
 
-    pub fn add_from_angle(&mut self, a: f32, mag: f32)
-    { self.x += a.cos()*mag; self.y += a.sin()*mag}
+    pub fn sub(&self, v2: &Vector2) -> Vector2
+    { return Vector2 {x: self.x - v2.x, y: self.y - v2.y}; }
 
-    pub fn sub_from_angle(&mut self, a: f32, mag: f32)
+    pub fn to_scale(&mut self, scaler: f32) -> Vector2
+    { return Vector2 {x: self.x * scaler, y: self.y * scaler}; }
+
+    pub fn add_self_from_angle(&mut self, a: f32, mag: f32)
+    { self.x += a.cos()*mag; self.y += a.sin()*mag;}
+
+    pub fn sub_self_from_angle(&mut self, a: f32, mag: f32)
     { self.x -= a.cos()*mag; self.y -= a.sin()*mag}
+
+    pub fn sub_from_angle(&mut self, a: f32, mag: f32) -> Vector2
+    { return Vector2 { x: self.x - a.cos()*mag, y: self.y - a.sin()*mag}; }
 
     pub fn get_x(&self) -> f32
     { return self.x; }
@@ -68,6 +78,11 @@ impl Vector2
 
     pub fn set_y(&mut self, y: f32)
     { self.y = y; }
+
+    pub fn neg1() -> Vector2
+    {
+         return Vector2 { x: -1f32, y: -1f32 }
+    }
 }
 
 #[wasm_bindgen]
@@ -80,7 +95,7 @@ pub enum ChargeType
 }
 
 #[wasm_bindgen]
-#[derive(Clone, Copy)]
+// #[derive(Clone, Copy)]
 pub struct Charge
 {
     pos: Vector2,
@@ -114,34 +129,140 @@ impl Charge
 }
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+#[derive(Clone, Copy)]
+pub struct Simulation
+{
+    record_points: u32,
+    step_distance: f32,
+    record_steps: u32,
+    charge_density: f32,
+    close_spawn_distance: f32,
+    point_distance: f32,
+    k: f32,
+}
+
+impl Simulation {
+    pub fn get_field_line_count(&self, charge: &Charge) -> u32
+    {
+        return if charge.q > 0f32 {(self.charge_density*charge.q).ceil() as u32} else {0};
+    }
+
+    pub fn get_field_start_points(&self, charge: &Charge) -> Vec<Vector2>
+    {
+        let point_count: f32 = self.get_field_line_count(charge) as f32;
+        let mut points: Vec<Vector2> = vec![Vector2::neg1(); point_count as usize];
+
+        for i in 0..(point_count as u32) 
+        {
+            let a: f32 = 2f32 * PI * (i as f32) / point_count;
+            points[i as usize] = Vector2 {
+                x: charge.pos.x + a.cos() * self.close_spawn_distance,
+                y: charge.pos.y + a.sin() * self.close_spawn_distance,
+            }
+        }
+
+        return points;
+    }
+
+    pub fn get_field(&self, charges: &Vec<Charge>, pos: &Vector2) -> Vector2
+    {
+        let mut field: Vector2 = Vector2 { x: 0f32, y: 0f32 };
+        for charge in charges.iter() {
+            match charge.charge_type {
+                ChargeType::Point=>{
+                    let a: f32   = f32::atan2(charge.pos.y - pos.y, charge.pos.x-pos.x);
+                    let mag: f32 = charge.q/f32::hypot(charge.pos.y - pos.y, charge.pos.x-pos.x);
+                    field.add_self_from_angle(a, mag);
+                }, 
+                ChargeType::Sphere=>{
+                    
+                }, 
+                ChargeType::Line=>{
+                    
+                }, 
+            }
+        }
+
+        return field.to_scale(self.k);
+    }
+
+    pub fn in_negative_charge(&self, charges: &Vec<Charge>, pos: &Vector2) -> bool
+    {
+        for charge in charges {
+            if Vector2::sub(&charge.pos, pos).mag() < self.point_distance
+            { return true; }
+        }
+        return false;
+    }
+
+    pub fn generate_field_line(&self, charges: &Vec<Charge>, start_pos: &Vector2) -> Vec<Vector2>
+    {
+        let mut points: Vec<Vector2> = vec![Vector2::neg1(); self.record_points as usize + 1];
+        let mut recent_points: Vec<Vector2> = vec![Vector2::neg1(); self.record_steps as usize];
+        let mut pos: Vector2 = *start_pos;
+
+        points.push(pos);
+
+        for _i in 0..self.record_points {
+            for j in 0..self.record_steps {
+                recent_points[j as usize] = pos;
+                pos.add_self(&self.get_field(charges, &pos).to_scale(self.step_distance));
+            }
+
+            let mut j: i32 = (self.record_steps - 1) as i32;
+            while self.in_negative_charge(charges, &pos) && j >= 0
+            {
+                pos = recent_points[j as usize];
+                j -= 1;
+            }
+
+            points.push(pos);
+        }
+
+        return points;
+    }
 }
 
 #[wasm_bindgen]
-pub fn greet(name: &str) -> u32{
-    console_log!("testing");
-    return 5;
-}
-
-pub fn get_field(charges: &Vec<Charge>, pos: &Vector2) -> Vector2
-{
-    let mut field: Vector2 = Vector2 { x: 0f32, y: 0f32 };
-    for charge in charges.iter() {
-        match charge.charge_type {
-            ChargeType::Point=>{
-                let a: f32   = f32::atan2(charge.pos.y - pos.y, charge.pos.x-pos.x);
-                let mag: f32 = charge.q/f32::hypot(charge.pos.y - pos.y, charge.pos.x-pos.x);
-                field.add_from_angle(a, mag);
-            }, 
-            ChargeType::Sphere=>{
-                
-            }, 
-            ChargeType::Line=>{
-                
-            }, 
+impl Simulation {
+    #[wasm_bindgen(constructor)]
+    pub fn new(record_points: u32,
+        step_distance: f32,
+        record_steps: u32,
+        charge_density: f32,
+        close_spawn_distance: f32,
+        point_distance: f32,
+        k: f32) -> Simulation
+    {
+        return Simulation {
+            record_points,
+            step_distance,
+            record_steps,
+            charge_density,
+            close_spawn_distance,
+            point_distance,
+            k
         }
     }
 
-    return field;
+    pub fn create_all_field_lines(arr: Array) //-> Vec<Vec<Vector2>>
+    {
+        let arr: js_sys::Array = arr.into();
+        // Extracting charges from the array
+        let mut charges: Vec<Charge> = Vec::with_capacity(arr.length() as usize);
+        for i in 0..arr.length() {
+            let js_object = arr.get(i).dyn_into::<js_sys::Object>().unwrap();
+            let q: f32 = Reflect::get(&js_object, &JsValue::from_str("id")).unwrap().as_f64().unwrap() as f32;
+
+            console_log!("Charge {} with a value of {}", i, q);
+        }
+        // let mut line_count: u32 = 0;
+        // for charge in charges
+        // {
+        //     line_count += get_field_line_count(sim_data, charge);
+        // }
+
+
+        
+    }
 }
